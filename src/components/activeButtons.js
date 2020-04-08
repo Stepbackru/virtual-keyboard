b7237e77bc6ee58ee796dcd80392bdf7c98e6302
Stepbackru <alex-ruba@bk.ru>
2020-04-08 22:10:14 +0300

    refactor: optimized functions of backlights

--- src/components/activeButtons.js
+++ src/components/activeButtons.js
@@ -1,10 +1,9 @@
 import buttons from '../data/buttons.js';
 
 const activeButtons = () => {
-  const lineItems = [...document.querySelectorAll('.keyboard__key')];
-  addActiveClassOnPressKey(lineItems);
-  removeActiveClass(lineItems);
   activeClickButton();
+  addActiveClassOnPressKey();
+  removeActiveClass();
 };
 
 const activeClickButton = () => {
@@ -16,24 +15,29 @@ const activeClickButton = () => {
     }
   });
 };
+
 const addActiveClassOnPressKey = () => {
   document.addEventListener('keydown', (e) => {
-    for (let i = 0; i < buttons.length; i++) {
-      if (e.code === buttons[i].code) {
-        buttons[i].lineItem.classList.add('keyboard__key-active');
-      }
-    }
+    iterationButtons(e, 'add', 'keyboard__key-active');
   });
 };
 
 const removeActiveClass = () => {
   document.addEventListener('keyup', (e) => {
-    for (let i = 0; i < buttons.length; i++) {
-      if (e.code === buttons[i].code) {
-        buttons[i].lineItem.classList.remove('keyboard__key-active');
+    iterationButtons(e, 'remove', 'keyboard__key-active');
+  });
+};
+
+const iterationButtons = (e, action, className) => {
+  for (let i = 0; i < buttons.length; i++) {
+    if (e.code === buttons[i].code) {
+      if (action === 'add') {
+        buttons[i].lineItem.classList.add(`${className}`);
+      } else if (action === 'remove') {
+        buttons[i].lineItem.classList.remove(`${className}`);
       }
     }
-  });
+  }
 };
 
 export default activeButtons;
